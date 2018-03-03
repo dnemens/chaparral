bbc5ac89e80395404239f2c5dd63f43d7e6d3eb6
dnemens <35116826+dnemens@users.noreply.github.com>
2018-03-02 16:37:34 -0800

    aiaag

--- center data/scripts/nmds.cent.R
+++ center data/scripts/nmds.cent.R
@@ -2,32 +2,29 @@
 
 library(vegan)
 
-#loads response matrix - absolute cover for each spp
-cover2 <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverAct.csv", header = T)
+#loads response matrix - RELATIVE cover for each spp
+cover2 <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverAct.no0.csv", header = T)
 
 #loads explan matrix (rdnbr, severity combos)
-cover1 <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccover1.csv", header = T)
+cover1 <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccover1.no0.csv", header = T)
 
 #creates vector for severity combo category
 cat <- cover1$SC
 
 #loads dat frame with dominant species for each plot
-domin <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.csv", header = T)
+domin <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.no0.csv", header = T)
 dom <- domin$abun
 
 #relativize cover data based on row max and plot total
 #cover2.stand <- wisconsin(cover2)
 #remove less common species and plots with no data
 #cover2 <- cover2[,1:4]
-#cover2 <- cover2[rowSums(cover2)!=0,]
 
-#relativize cover data
-#cover2.Rel <- decostand(cover2, method = "total")
 #adds dummy species with cover=.01 for BC distance meaure
-cover2$fake <- .01
+#cover2$fake <- .01
 
 #MDS
-z <- metaMDS(comm = cover2, distance = "bray", k=2, weakties=T, trymax = 50)
+z <- metaMDS(comm = cover2, k=2, distance = "bray", weakties=T, trymax = 100, autotransform = FALSE)
 
 stressplot(z, lwd=2)
 z$stress
@@ -38,7 +35,7 @@ plot(z, display="sites")
 points(z, col = dom, pch=20, cex=1.5)
 
 #overlays storrie and chips severity values onto ordination
-ordi <- ordisurf(z, cover1$storrie_rdnbr, main="Burn severity")
+ordi <- ordisurf(z, cover1$chips_rdnbr, main="Burn severity")
 #adds points, color-coded by dominant species
 points(z, col = dom, pch=20, cex=1.5)
 
@@ -49,12 +46,7 @@ str(ordi.grid) #it's a list though - cannot be plotted as is
 ordi.mite <- expand.grid(x = ordi.grid$x, y = ordi.grid$y) #get x and ys
 ordi.mite$z <- as.vector(ordi.grid$z) #unravel the matrix for the z scores
 ordi.mite.na <- data.frame(na.omit(ordi.mite)) #gets rid of the nas
-ordi.mite.na #looks ready for plotting!
 
-#rotates axes according to storrie severity (continuous)
-z.sev <- MDSrotate(z, cover1$storrie_rdnbr)
-plot(z.sev, display = "sites")
-points(z.sev$points, col = cat, pch=16)
 
 #or ggplot
 gz <- data.frame(dom, scores(z), cover1$storrie_rdnbr, cover1$chips_rdnbr)
@@ -62,12 +54,41 @@ gz <- na.omit(gz)
   
 library(ggplot2)
 library(directlabels)
-ggplot(gz, aes(x=NMDS1, y=NMDS2))  +
+Sto <- ggplot(gz, aes(x=NMDS1, y=NMDS2))  +
   geom_point(aes(shape=dom), size=3)+
   theme_classic()+
   stat_contour(data = ordi.mite.na, aes(x = x, y = y, z = z, colour = ..level..), cex=1.1)+
-  scale_color_gradient(low="green", high="red")
+  scale_color_gradient(low = "green", high ="red")+
+  labs(colour="Fire severity\n(RdNBR)", title="Storrie Fire")+
+  theme(plot.title = element_text(hjust=.5, size=15))
+
+Chip <- ggplot(gz, aes(x=NMDS1, y=NMDS2))  +
+  geom_point(aes(shape=dom), size=3)+
+  theme_classic()+
+  stat_contour(data = ordi.mite.na, aes(x = x, y = y, z = z, colour = ..level..), cex=1.1)+
+  scale_color_gradient(low = "green", high ="red")+
+  labs(colour="Fire severity\n(RdNBR)", title="Chips Fire")+
+  theme(plot.title = element_text(hjust=.5, size=15))
+
+library(grid)
+library(gridExtra)
+
+#creates uniform widths for plots
+Sto <- ggplot_gtable(ggplot_build(Sto))
+maxWidth = unit.pmax(Chip$widths[3:3], Sto$widths[3:3])
+Chip$widths[3:3] <- maxWidth
+Sto$widths[3:3] <- maxWidth
+
+#stack both ggplots
+fine <- grid.arrange(Sto, Chip, nrow=1, ncol=2)
 
 
 setwd("/Users/dnemens/Dropbox/CBO/chaparral/plots")
-ggsave(filename = "ggordi_storrie.jpg")
+ggsave(fine, filename = "ggordi_sidebyside.jpg")
+
+##################################################################
+#rotates axes according to storrie severity (continuous)
+z.sev <- MDSrotate(z, cover1$storrie_rdnbr)
+plot(z.sev, display = "sites")
+points(z.sev$points, col = cat, pch=16)
+
--- center data/scripts/prep.center.R
+++ center data/scripts/prep.center.R
@@ -1,62 +1,6 @@
 #preps center sub-plot data for analysis, creates data files of response data
 library(tidyverse)
 
-cover.cent <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/center sub plot.csv", header = T)
-
-#summarizes data sheet, giving total crown area per species for each plot
-ccover.sum <- cover.cent %>%
-  group_by(plot, Spp) %>%
-  summarize(cover = sum(crown.area))
-
-ccover <- spread(ccover.sum, key = "Spp", value = "cover", fill = 0.0)
-
-#creates a species-only matrix (response)
-
-#removes columns: plot, trees and v1
-ccover1 <- as.data.frame(ccover [, -c(1,2, 3, 7, 25:32)]) 
-#removes rare species
-library(labdsv)
-ccover2 <- vegtab(taxa = ccover1, minval = (.05*nrow(ccover1)))
-
-#creates data file of reponse matrix with most common species >5% frequency
-write.csv(ccover2, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverRaw.csv", row.names = F)
-
-#calculates actual PERCENT cover for each species
-ccoverAct <- ccover2/5648
-write.csv(ccoverAct, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverAct.csv", row.names = F)
-
-
-#relativizes by row totals - gives RELATIVE cover for each species
-library(vegan)
-ccoverRel <- decostand(ccover2, method = "total")
-
-#adds dummy species with cover=1 for distance meaures
-#ccover2$fake <- 1
-
-#creates data file of RELATIVE COVER reponse matrix with most common species >5% frequency
-write.csv(ccoverRel, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverRel.csv", row.names = F)
-
-########################################################################################
-#creates data frame with single response for each plot -- most common species in plot by cover
-#finds species with highest cover for each plot, adds a column with that species' name
-most.abundant2 <- mapply(function(y)
-{a <- which(ccoverRel[y,] == max(ccoverRel[y,]), arr.ind=T)
-names(ccoverRel[a[,2]])} , 1:length(ccoverRel[,1]))
-
-#add an NA if more than one species is dominant
-most.abundant3 <- mapply(function(y){
-  if(length(most.abundant2[[y]]) > 1)
-  {NA} else
-  {most.abundant2[[y]]}
-}, 1:length(most.abundant2))
-
-#creates a new data frame with abundance codes for each plot, merged with rdnbr values
-rdnbr <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/rdnbr.csv")
-
-cdomin <- data.frame(rdnbr, abun = most.abundant3)
-
-#saves data frame as new spreadsheet
-write.csv(cdomin, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.csv", row.names = F)
 #######################################################################
 #creates a matrix of predictor variables
 ##creates Storrie and Chips categorical severity from plot # designations
@@ -83,4 +27,91 @@ cover.sub <- cover.sub %>%
 cover1 <- cover.sub[,-3]
 
 write.csv(cover1, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccover1.csv", row.names = F)
+#######################################################################################
+
+cover.cent <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/center sub plot.csv", header = T)
+
+#summarizes data sheet, giving total crown area per species for each plot
+ccover.sum <- cover.cent %>%
+  group_by(plot, Spp) %>%
+  summarize(cover = sum(crown.area))
+
+ccover <- spread(ccover.sum, key = "Spp", value = "cover", fill = 0.0)
+
+#creates a species-only matrix (response)
+
+#removes columns: plot, trees and v1
+ccover1 <- as.data.frame(ccover [, -c(1,2, 3, 7, 25:32)]) 
+#removes rare species
+library(labdsv)
+ccover2 <- vegtab(taxa = ccover1, minval = (.05*nrow(ccover1)))
+
+#creates data file of reponse matrix with most common species >5% frequency
+write.csv(ccover2, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverRaw.csv", row.names = F)
+
+#calculates actual PERCENT cover for each species
+ccoverAct <- ccover2/5648
+write.csv(ccoverAct, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverAct.csv", row.names = F)
+
+#relativizes by row totals - gives RELATIVE cover for each species
+library(vegan)
+ccoverRel <- decostand(ccover2, method = "total")
+
+#creates data file of RELATIVE COVER reponse matrix with most common species >5% frequency
+write.csv(ccoverRel, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverRel.csv", row.names = F)
+
+#########################################################
+#removes blank rows!!  
+Nozero <- data.frame(ccoverAct, cover1)
+Nozero2 <- Nozero[rowSums(Nozero[,1:7])!=0,]
+
+ccoverAct.no0 <- Nozero2[,1:7]
+ccover1.no0 <- Nozero2[,8:11]
+
+write.csv(ccoverAct.no0, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccoverAct.no0.csv", row.names = F)
+write.csv(ccover1.no0, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/ccover1.no0.csv", row.names = F)
+
+
+########################################################################################
+#creates data frame with single response for each plot -- most common species in plot by cover
+#finds species with highest cover for each plot, adds a column with that species' name
+most.abundant2 <- mapply(function(y)
+{a <- which(ccoverRel[y,] == max(ccoverRel[y,]), arr.ind=T)
+names(ccoverRel[a[,2]])} , 1:length(ccoverRel[,1]))
+
+#add an NA if more than one species is dominant
+most.abundant3 <- mapply(function(y){
+  if(length(most.abundant2[[y]]) > 1)
+  {NA} else
+  {most.abundant2[[y]]}
+}, 1:length(most.abundant2))
+
+#creates a new data frame with abundance codes for each plot, merged with rdnbr values
+rdnbr <- read.csv("C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/rdnbr.csv")
+
+cdomin <- data.frame(rdnbr, abun = most.abundant3)
+
+#saves data frame as new spreadsheet
+write.csv(cdomin, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.csv", row.names = F)
+
+################################################################
+#same as above, removing plots with 0's
+
+#creates data frame with single response for each plot -- most common species in plot by cover
+#finds species with highest cover for each plot, adds a column with that species' name
+most.abundant2 <- mapply(function(y)
+{a <- which(ccoverRel.no0[y,] == max(ccoverRel.no0[y,]), arr.ind=T)
+names(ccoverRel.no0[a[,2]])} , 1:length(ccoverRel.no0[,1]))
+
+#add an NA if more than one species is dominant
+most.abundant3 <- mapply(function(y){
+  if(length(most.abundant2[[y]]) > 1)
+  {NA} else
+  {most.abundant2[[y]]}
+}, 1:length(most.abundant2))
+
+cdomin <- data.frame(cover1.no0, abun = most.abundant3)
+
+#saves data frame as new spreadsheet
+write.csv(cdomin, file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.no0.csv", row.names = F)
 

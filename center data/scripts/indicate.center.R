7f6a2f9b99443fe9b9b7b5043c454ca4068d001a
dnemens <35116826+dnemens@users.noreply.github.com>
2018-05-03 17:06:21 -0700

    go!

--- center data/scripts/indicate.center.R
+++ center data/scripts/indicate.center.R
@@ -1,61 +1,65 @@
-#indicator species analysis of center sub-plot data  
+#indicator species analysis of center sub data  
 library(tidyverse)
-library(vegan)
-library(indicspecies)
-library(labdsv)
 
-#data frame of response variables (all common species Relative cover values)
-cover2 <- read.csv(file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/coverRel.csv")
+#data frame of response variables (ALL common species importance values)
+cover2 <- read.csv(file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/importance.csv")
 # dataframe of predictor variables (rdnbr, plot names & categories)
 cover <- read.csv(file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cover1.csv")
-#add dominant species for each plot
-dom <- read.csv(file="C:/Users/dnemens/Dropbox/CBO/chaparral/center data/data sheets/cdomin.csv")
 
 #creates vector of storrie/chips severity combination
 cat <- as.factor(cover$SC)
 ##############################################################################################
 #indicator species analysis using field-determined severity combinations####
+library(indicspecies)
 cov.ind <- multipatt(cover2, cluster = cat, control = how(nperm = 999), duleg = T)
-summary(cov.ind, indvalcomp = T)
+cov.ind$sign
+summary(cov.ind)
 
 #extracts indicator value for each significant species
+library(labdsv)
 ind1 <- indval(cover2, cat)
 ind1$indval
-summary(ind1, alpha=1)
+summary(ind1)
+############################################################################################
+#combine indicator species analysis with k means clustering####
+#choose # of groups
+ck <- cascadeKM(cover2, inf.gr = 2, sup.gr = 12) #evaluates # of groups 
+plot(ck) #indicates that 4 groups are best?
 
+#conduct a kmeans cluster analysis
+k <- kmeans(cover2, centers = 4, nstart = 100)
+
+#runs combined analysis 
+#indicator species analysis with kmeans cluster determining groups
+kind <- multipatt(cover2, k$cluster, control = how(nperm = 999))
+summary(kind, indvalcomp = TRUE)
+
+#should I use the pam function, since the distance matrix above may not be appropriate?  
+library(cluster)
+k4.pam <- pam(x=cover2, metric = "euclidean", k=4)
+summary(k4.pam)
 ###############################################################
 #creates NMDS ordination####
-z <- metaMDS(comm = cover2, k=3, distance = "horn", weakties=T, trymax = 100, autotransform = FALSE)
+library(vegan)
+z <- metaMDS(comm = cover2, k=4, distance = "horn", weakties=T, trymax = 100, autotransform = FALSE)
 z$stress
+plot(z, display = "sites")
+points(z, pch=20, col=k$cluster, cex=2)
+ordihull(z, groups = k$cluster, col=k$cluster)
 
-###########
+################################################################
 #rotate NMDS by storrie severity####
 z.sev <- MDSrotate(z, cover$storrie_rdnbr)
-
-#plot
 plot(z.sev, display = "sites")
-
-#code points by dominant species
-cols <- rainbow(12)
-palette(col.rainbow)
-points(z.sev$points, bg=dom$abun, pch=21, cex=1.3)
-text(.1,.5, "CEIN", cex=1.2, col="black") 
-text(.5,-.7, "CECO", cex=1.2, col="black") 
-text(-.5,-.75, "ABCO/QUKE", cex=1.2, col="black") 
-title(main = "NMS with dominant species providing color coding")
+#code points by cluster from kmeans
+points(z.sev$points, col=k$cluster, pch=20, cex=2) 
+ordihull(z.sev, groups = k$cluster, col=c("black", "red", "green", "blue"))
+title(main = "Rotated with Storrie on X")
 
 #add continuous severity vectors
-sev.fit <- envfit(z.sev ~ storrie_rdnbr + chips_rdnbr , data=cover, na.rm=T)
-#aspect and elevation were not signif
-#overlays vectors onto rotated nmds
+sev.fit <- envfit(z.sev ~ storrie_rdnbr + chips_rdnbr, data=cover)
+#overlays chips severity as vectors onto rotated nmds
 plot(sev.fit)
-sev.fit$vectors
 
-# add circles around clusters of points  
-library(plotrix)
-draw.ellipse(-.1, .6, a=.5, b=.25, border = "black", lwd=2) #cein
-draw.ellipse(.75, -.75, a=.5, b=.18, border = "black", lwd=2) #ceco
-draw.ellipse(-.6, -.85, a=.7, b=.2, border = "black", lwd=2) #abco/quke
-#####################################################
-#plot with ggplot
 
+ordicluster(ord=sev.fit, display = "sites", cluster=k, prune=3)
